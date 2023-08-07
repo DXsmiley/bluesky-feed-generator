4f422de7a2633c789d21820da03471a2d676fb32
DXsmiley <dxsmiley@hotmail.com>
2023-08-07 22:15:02 +1000

    Scrape bsky for recent furry posts onstartup, to quickly populate the feed

--- server/load_known_furries.py
+++ server/load_known_furries.py
@@ -1,7 +1,20 @@
-from server.database import Actor, db
-import json
+from datetime import datetime, timezone, timedelta
+
+import server.monkeypatch
+
+from server.database import db
+
+from atproto import Client
+
+from publish_feed import HANDLE, PASSWORD
+
+from typing import Iterable, List, Optional, Dict
+
+from atproto.xrpc_client.models.app.bsky.actor.defs import ProfileView, ProfileViewDetailed
+from atproto.xrpc_client.models.app.bsky.feed.defs import FeedViewPost
+
 import re
-from typing import Any, Optional
+import json
 
 
 def is_girl(description: Optional[str]) -> bool:
@@ -29,30 +42,137 @@ def is_girl(description: Optional[str]) -> bool:
     return False
 
 
-def load():
-    with open('known_furries.json') as f:
-        blob = json.load(f)
+def simplify_profile_view(p: ProfileViewDetailed) -> ProfileView:
+    return ProfileView(
+        did=p.did,
+        handle=p.handle,
+        avatar=p.avatar,
+        description=p.description,
+        displayName=p.displayName,
+        indexedAt=p.indexedAt,
+        labels=p.labels,
+        viewer=p.viewer
+    )
 
-    db.post.delete_many()
-    db.actor.delete_many()
 
-    for x, i in enumerate(blob['furries']):
-        if x % 100 == 0:
-            print(f'Loading furries into DB: {x} / {len(blob["furries"])}')
-        db.actor.create(
+def get_followers(client: Client, did: str) -> Iterable[ProfileView]:
+    r = client.bsky.graph.get_followers({'actor': did})
+    yield from r.followers
+    while r.cursor:
+        r = client.bsky.graph.get_followers({'actor': did, 'cursor': r.cursor})
+        yield from r.followers
+
+
+def get_follows(client: Client, did: str) -> Iterable[ProfileView]:
+    r = client.bsky.graph.get_follows({'actor': did})
+    yield from r.follows
+    while r.cursor:
+        r = client.bsky.graph.get_follows({'actor': did, 'cursor': r.cursor})
+        yield from r.follows
+
+
+def get_posts(client: Client, did: str, after: datetime) -> Iterable[FeedViewPost]:
+    r = client.bsky.feed.get_author_feed({'actor': did})
+    for i in r.feed:
+        if datetime.strptime(i.post.record['createdAt'], r'%Y-%m-%dT%H:%M:%S.%fZ') < after:
+            return
+        yield i
+    while r.cursor:
+        r = client.bsky.feed.get_author_feed({'actor': did, 'cursor': r.cursor})
+        for i in r.feed:
+            if datetime.strptime(i.post.record['createdAt'], r'%Y-%m-%dT%H:%M:%S.%fZ') < after:
+                return
+            yield i
+
+
+def load() -> None:
+    client = Client()
+    client.login(HANDLE, PASSWORD)
+
+    only_posts_after = datetime.now() - timedelta(hours=50)
+
+    # Accounts picked because they're large and the people following them
+    # are most likely furries. Doesn't matter 
+    known_furries_handles = [
+        'puppyfox.bsky.social',
+        'furryli.st',
+        'braeburned.com',
+        '100racs.bsky.social',
+        'glitzyfox.bsky.social',
+        'itswolven.bsky.social',
+        'coolkoinu.bsky.social',
+        'gutterbunny.bsky.social',
+        'zoeydogy.bsky.social'
+    ]
+
+    print('Grabbing known furries...')
+
+    furries: Dict[str, ProfileView] = {}
+
+    for handle in known_furries_handles:
+        print('Known furry:', handle)
+        profile = simplify_profile_view(client.bsky.actor.get_profile({'actor': handle}))
+        furries[profile.did] = profile
+        for follower in get_followers(client, profile.did):
+            print('Follower:', follower.handle, flush=False)
+            furries[follower.did] = follower
+
+    print('Adding furries to database')
+
+    for user in furries.values():
+        print(user.handle, flush=False)
+        db.actor.upsert(
+            where={'did': user.did},
             data={
-                'did': i['did'],
-                'handle': i['handle'],
-                'description': i['description'],
-                'displayName': i['displayName'],
-                'in_fox_feed': True,
-                'in_vix_feed': is_girl(i['description']),
+                'create': {
+                    'did': user.did,
+                    'handle': user.handle,
+                    'description': user.description,
+                    'displayName': user.displayName,
+                    'in_fox_feed': True,
+                    'in_vix_feed': is_girl(user.description),
+                },
+                'update': {
+                    'did': user.did,
+                    'handle': user.handle,
+                    'description': user.description,
+                    'displayName': user.displayName,
+                    'in_fox_feed': True,
+                    'in_vix_feed': is_girl(user.description),
+                }
             }
         )
 
-    print('\ndone loading furries')
+    print('Grabbing posts for database')
 
+    for user in sorted(furries.values(), key=lambda i: is_girl(i.description), reverse=True):
+        print(user.handle, flush=False)
+        for post in get_posts(client, user.did, only_posts_after):
+            p = post.post
+            # this filters out furry reposts from non-furry accounts
+            # not an intentional choice but we violate the foreign key otherwise lmao
+            if db.actor.find_unique({'did': p.author.did}) is not None:
+                db.post.upsert(
+                    where={'uri': p.uri},
+                    data={
+                        'create': {
+                            'uri': p.uri,
+                            'cid': p.cid,
+                            # TODO: Fix these
+                            'reply_parent': None, # None if post.reply is None else post.reply.parent,
+                            'reply_root': None, # None if post.reply is None else post.reply.root,
+                            'indexed_at': datetime.strptime(p.record['createdAt'], r'%Y-%m-%dT%H:%M:%S.%fZ'),
+                            'like_count': p.likeCount or 0,
+                            'authorId': p.author.did,
+                        },
+                        'update': {
+                            'like_count': p.likeCount or 0,
+                        }
+                    }
+                )
+
+    print('Done')
 
 if __name__ == '__main__':
     load()
-    
+
