f2f289150f60fa50156802787019545f49770470
DXsmiley <dxsmiley@hotmail.com>
2023-08-07 16:28:22 +1000

    publish multiple feeds and also don't take creds from the source code itself

--- publish_feed.py
+++ publish_feed.py
@@ -2,79 +2,70 @@
 # YOU MUST INSTALL ATPROTO SDK
 # pip3 install atproto
 
+import os
+import dotenv
 from datetime import datetime
-
 from atproto.xrpc_client.models import ids
-
 from atproto import Client, models
+from typing import Optional
+
+dotenv.load_dotenv()
 
 # YOUR bluesky handle
 # Ex: user.bsky.social
-HANDLE: str = ''
+HANDLE: str = os.environ['HANDLE']
 
 # YOUR bluesky password, or preferably an App Password (found in your client settings)
 # Ex: abcd-1234-efgh-5678
-PASSWORD: str = ''
+PASSWORD: str = os.environ['PASSWORD']
 
 # The hostname of the server where feed server will be hosted
 # Ex: feed.bsky.dev
-HOSTNAME: str = ''
+HOSTNAME: str = os.environ['HOSTNAME']
 
-# A short name for the record that will show in urls
-# Lowercase with no spaces.
-# Ex: whats-hot
-RECORD_NAME: str = ''
-
-# A display name for your feed
-# Ex: What's Hot
-DISPLAY_NAME: str = ''
-
-# (Optional) A description of your feed
-# Ex: Top trending content from the whole network
-DESCRIPTION: str = 'powered by The AT Protocol SDK for Python'
-
-# (Optional) The path to an image to be used as your feed's avatar
-# Ex: ./path/to/avatar.jpeg
-AVATAR_PATH: str = ''
 
 # (Optional). Only use this if you want a service did different from did:web
-SERVICE_DID: str = ''
+SERVICE_DID: Optional[str] = None
 
 
-# -------------------------------------
-# NO NEED TO TOUCH ANYTHING BELOW HERE
-# -------------------------------------
+def register(client: Client, record_name: str, display_name: str, description: str, avatar_path: Optional[str]):
+    feed_did = SERVICE_DID if SERVICE_DID is not None else f'did:web:{HOSTNAME}'
 
-
-def main():
-    client = Client()
-    client.login(HANDLE, PASSWORD)
-
-    feed_did = SERVICE_DID
-    if not feed_did:
-        feed_did = f'did:web:{HOSTNAME}'
+    avatar_path = None
 
     avatar_blob = None
-    if AVATAR_PATH:
-        with open(AVATAR_PATH, 'rb') as f:
+    if avatar_path:
+        with open(avatar_path, 'rb') as f:
             avatar_data = f.read()
             avatar_blob = client.com.atproto.repo.upload_blob(avatar_data).blob
 
     response = client.com.atproto.repo.put_record(models.ComAtprotoRepoPutRecord.Data(
         repo=client.me.did,
         collection=ids.AppBskyFeedGenerator,
-        rkey=RECORD_NAME,
+        rkey=record_name,
         record=models.AppBskyFeedGenerator.Main(
             did=feed_did,
-            displayName=DISPLAY_NAME,
-            description=DESCRIPTION,
+            displayName=display_name,
+            description=description,
             avatar=avatar_blob,
             createdAt=datetime.now().isoformat(),
         )
     ))
 
-    print('Successfully published!')
-    print('Feed URI (put in "WHATS_ALF_URI" env var):', response.uri)
+    return response.uri
+
+
+def main():
+    client = Client()
+    client.login(HANDLE, PASSWORD)
+    
+    feeds = [
+        ('fox-feed', '🦊 Main', 'Custom algorithmic feed for furry posts (in development)', './fox.png'),
+        ('vix-feed', '🦊 Vix', 'Further personalised version of 🦊 Main', './fox.png'),
+    ]
+
+    for record_name, display_name, description, avatar_path in feeds:
+        register(client, record_name, display_name, description, avatar_path)
 
 
 if __name__ == '__main__':
