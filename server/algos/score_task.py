ffc1c58eb2f855c93e90b29382aa375e4d3df90a
DXsmiley <dxsmiley@hotmail.com>
2023-08-31 00:28:38 +1000

    debug stuff

--- server/algos/score_task.py
+++ server/algos/score_task.py
@@ -168,6 +168,7 @@ async def create_feed(db: Database, fp: FeedParameters, rd: RunDetails) -> None:
                     'feed_name': fp.feed_name,
                 }
             )
+            print('Stored!', fp.feed_name, rd.run_version, post.post.uri)
         except prisma.errors.UniqueViolationError:
             uri_count = sum(i.post.uri == post.post.uri for _, i in will_store)
             cprint(f'Unique PostScore violation error on {post.post.uri}::{fp.feed_name}::{rd.run_version} ({uri_count} instances of this URI)', 'red', force_color=True)
