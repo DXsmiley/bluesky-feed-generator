da09809ed993f19a77fe069c930cdca7b9fda438
DXsmiley <dxsmiley@hotmail.com>
2023-09-04 22:33:40 +1000

    split the firehose onto a separate dyno

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: ./h-setup && PORT=$PORT DATABASE_URL=$DATABASE_URL python -m server --admin-panel
+release: prisma db push --accept-data-loss
+web: PORT=$PORT DATABASE_URL=$DATABASE_URL python -m server --admin-panel --no-firehose
+firehose: python -m server.firehose
---
+++ server/firehose.py
@@ -0,0 +1,13 @@
+import asyncio
+from server import data_stream, config
+from server.database import make_database_connection
+from server.data_filter import operations_callback
+
+
+async def main():
+    db = await make_database_connection()
+    await data_stream.run(db, config.SERVICE_DID, operations_callback, None)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
