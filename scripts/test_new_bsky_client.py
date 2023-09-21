aa718836838f58746e1dfb9017677b7b70c9ed6f
DXsmiley <dxsmiley@hotmail.com>
2023-09-22 01:02:33 +1000

    forgot to add this earlier

---
+++ scripts/test_new_bsky_client.py
@@ -0,0 +1,13 @@
+import asyncio
+from server.database import make_database_connection
+from server.bsky import make_bsky_client, get_followers
+
+async def main():
+    db = await make_database_connection()
+    client = await make_bsky_client(db)
+    async for i in get_followers(client, client.me.did):
+        print(i.handle, i.display_name)
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
