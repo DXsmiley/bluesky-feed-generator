5e6bddd8e02e9ab2948f4970404e97fc3611dc70
DXsmiley <dxsmiley@hotmail.com>
2023-09-28 21:30:24 +1000

    monkeypatch login with handle + password

--- foxfeed/monkeypatch.py
+++ foxfeed/monkeypatch.py
@@ -1,6 +1,17 @@
-# from typing_extensions import Literal
-# import atproto.xrpc_client.models.app.bsky.graph.defs
+from atproto.xrpc_client.models.com.atproto.server import create_session
+from atproto.xrpc_client.models import base
+from pydantic import Field
+import typing as t
 
-# atproto.xrpc_client.models.app.bsky.graph.defs.ListPurpose = Literal[
-#     "app.bsky.graph.defs#modlist"
-# ]
+class Response(base.ResponseModelBase):
+
+    """Output data model for :obj:`com.atproto.server.createSession`."""
+
+    access_jwt: str = Field(alias='accessJwt')  #: Access jwt.
+    did: str  #: Did.
+    handle: str  #: Handle.
+    refresh_jwt: str = Field(alias='refreshJwt')  #: Refresh jwt.
+    email: t.Optional[str] = None  #: Email.
+    emailConfirmed: t.Optional[bool] = None
+
+create_session.Response = Response
