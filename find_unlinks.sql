49b390b648dcc436b8c4be2c64b72bf47c87f39b
DXsmiley <dxsmiley@hotmail.com>
2023-10-12 22:37:12 +1100

    make sure unlink query doesn't overload the server with too many results

--- find_unlinks.sql
+++ find_unlinks.sql
@@ -1,4 +1,7 @@
-SELECT p1.embed_uri AS uri FROM "Post" as p1 LEFT OUTER JOIN "Post" as p2 ON p1.embed_uri = p2.uri WHERE p2.uri IS NULL AND p1.embed_uri LIKE '%/app.bsky.feed.post/%'
-UNION SELECT p1.reply_root AS uri FROM "Post" as p1 LEFT OUTER JOIN "Post" as p2 ON p1.reply_root = p2.uri WHERE p2.uri IS NULL AND p1.reply_root LIKE '%/app.bsky.feed.post/%'
-UNION SELECT p1.reply_parent AS uri FROM "Post" as p1 LEFT OUTER JOIN "Post" as p2 ON p1.reply_parent = p2.uri WHERE p2.uri IS NULL AND p1.reply_parent LIKE '%/app.bsky.feed.post/%'; 
-LIMIT 1000
+WITH t AS (
+    SELECT p1.embed_uri AS uri FROM "Post" as p1 LEFT OUTER JOIN "Post" as p2 ON p1.embed_uri = p2.uri WHERE p2.uri IS NULL AND p1.embed_uri LIKE '%/app.bsky.feed.post/%'
+    UNION SELECT p1.reply_root AS uri FROM "Post" as p1 LEFT OUTER JOIN "Post" as p2 ON p1.reply_root = p2.uri WHERE p2.uri IS NULL AND p1.reply_root LIKE '%/app.bsky.feed.post/%'
+    UNION SELECT p1.reply_parent AS uri FROM "Post" as p1 LEFT OUTER JOIN "Post" as p2 ON p1.reply_parent = p2.uri WHERE p2.uri IS NULL AND p1.reply_parent LIKE '%/app.bsky.feed.post/%'
+)
+
+SELECT * FROM t LIMIT 1000;
--- foxfeed/gen/db.py
+++ foxfeed/gen/db.py
@@ -215,10 +215,14 @@ async def score_by_interactions(
     return result
 
 find_unlinks_sql_query = """
-SELECT p1.embed_uri AS uri FROM "Post" as p1 LEFT OUTER JOIN "Post" as p2 ON p1.embed_uri = p2.uri WHERE p2.uri IS NULL AND p1.embed_uri LIKE '%/app.bsky.feed.post/%'
-UNION SELECT p1.reply_root AS uri FROM "Post" as p1 LEFT OUTER JOIN "Post" as p2 ON p1.reply_root = p2.uri WHERE p2.uri IS NULL AND p1.reply_root LIKE '%/app.bsky.feed.post/%'
-UNION SELECT p1.reply_parent AS uri FROM "Post" as p1 LEFT OUTER JOIN "Post" as p2 ON p1.reply_parent = p2.uri WHERE p2.uri IS NULL AND p1.reply_parent LIKE '%/app.bsky.feed.post/%'; 
-LIMIT 1000
+WITH t AS (
+    SELECT p1.embed_uri AS uri FROM "Post" as p1 LEFT OUTER JOIN "Post" as p2 ON p1.embed_uri = p2.uri WHERE p2.uri IS NULL AND p1.embed_uri LIKE '%/app.bsky.feed.post/%'
+    UNION SELECT p1.reply_root AS uri FROM "Post" as p1 LEFT OUTER JOIN "Post" as p2 ON p1.reply_root = p2.uri WHERE p2.uri IS NULL AND p1.reply_root LIKE '%/app.bsky.feed.post/%'
+    UNION SELECT p1.reply_parent AS uri FROM "Post" as p1 LEFT OUTER JOIN "Post" as p2 ON p1.reply_parent = p2.uri WHERE p2.uri IS NULL AND p1.reply_parent LIKE '%/app.bsky.feed.post/%'
+)
+
+SELECT * FROM t LIMIT 1000;
+
 """
 
 async def find_unlinks(
