7860a26c8c3c917b9dee6bc9a95e6b2232c6fce1
DXsmiley <dxsmiley@hotmail.com>
2023-10-14 00:06:23 +1100

    don't track unlinks outwards from out-of-network posts

--- foxfeed/gen/db.py
+++ foxfeed/gen/db.py
@@ -216,9 +216,27 @@ async def score_by_interactions(
 
 find_unlinks_sql_query = """
 WITH t AS (
-    SELECT p1.embed_uri AS uri FROM "Post" as p1 LEFT OUTER JOIN "Post" as p2 ON p1.embed_uri = p2.uri WHERE p2.uri IS NULL AND p1.embed_uri LIKE '%/app.bsky.feed.post/%'
-    UNION SELECT p1.reply_root AS uri FROM "Post" as p1 LEFT OUTER JOIN "Post" as p2 ON p1.reply_root = p2.uri WHERE p2.uri IS NULL AND p1.reply_root LIKE '%/app.bsky.feed.post/%'
-    UNION SELECT p1.reply_parent AS uri FROM "Post" as p1 LEFT OUTER JOIN "Post" as p2 ON p1.reply_parent = p2.uri WHERE p2.uri IS NULL AND p1.reply_parent LIKE '%/app.bsky.feed.post/%'
+    SELECT p1.embed_uri AS uri
+    FROM "Post" as p1
+    LEFT OUTER JOIN "Post" as p2 ON p1.embed_uri = p2.uri
+    LEFT OUTER JOIN "Actor" as author ON p1."authorId" = author.did
+    WHERE p2.uri IS NULL
+      AND p1.embed_uri LIKE '%/app.bsky.feed.post/%'
+      AND author.is_external_to_network IS FALSE
+    UNION SELECT p1.reply_root AS uri
+    FROM "Post" as p1
+    LEFT OUTER JOIN "Post" as p2 ON p1.reply_root = p2.uri
+    LEFT OUTER JOIN "Actor" as author ON p1."authorId" = author.did
+    WHERE p2.uri IS NULL
+      AND p1.reply_root LIKE '%/app.bsky.feed.post/%'
+      AND author.is_external_to_network IS FALSE
+    UNION SELECT p1.reply_parent AS uri
+    FROM "Post" as p1
+    LEFT OUTER JOIN "Post" as p2 ON p1.reply_parent = p2.uri
+    LEFT OUTER JOIN "Actor" as author ON p1."authorId" = author.did
+    WHERE p2.uri IS NULL
+      AND p1.reply_parent LIKE '%/app.bsky.feed.post/%'
+      AND author.is_external_to_network IS FALSE
 )
 
 SELECT * FROM t LIMIT 1000;
